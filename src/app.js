3d848503df3a7b16998ef600d182e2bda3ec6243
LiaXcod <lianunes.f@gmail.com>
2023-10-28 13:01:49 +0100

    introduced real data for city,T, wind, humidity

--- src/app.js
+++ src/app.js
@@ -1,6 +1,19 @@
 function displayTemperature(response) {
   console.log(response.data);
+
+  let temperatureElement = document.querySelector("#temp");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
+
 let apiKey = "8036c849d00de892979a86e56e32649b";
 let apiEndPoint = "https://api.openweathermap.org/data/2.5/weather?";
 let cityName = "London";
