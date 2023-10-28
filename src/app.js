59cdb58b27176e1cb83ac414afa2412c44a23be6
LiaXcod <lianunes.f@gmail.com>
2023-10-28 13:55:05 +0100

    introduced date (data&functions)

--- src/app.js
+++ src/app.js
@@ -1,6 +1,45 @@
-function displayTemperature(response) {
-  console.log(response.data);
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let weekDays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let weekDay = weekDays[date.getDay()];
 
+  let day = date.getDate();
+
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[date.getMonth()];
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${minutes}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${weekDay}, ${day}/${month}, ${hours}:${minutes}`;
+}
+function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temp");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
 
@@ -12,6 +51,9 @@ function displayTemperature(response) {
 
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "8036c849d00de892979a86e56e32649b";
